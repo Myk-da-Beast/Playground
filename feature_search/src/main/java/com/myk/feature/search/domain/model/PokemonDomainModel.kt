71fef59fb3d66b3b961438c50bbd1a49d6f60720
Myk da Beast <mykol00@gmail.com>
2020-11-25 23:46:24 -0800

    fixed `ComplexMethod` warning

--- feature_search/src/main/java/com/myk/feature/search/domain/model/PokemonDomainModel.kt
+++ feature_search/src/main/java/com/myk/feature/search/domain/model/PokemonDomainModel.kt
@@ -10,26 +10,33 @@ data class PokemonDomainModel(
     val types: List<String> = listOf()
 ) {
     @ColorRes
-    @Suppress("ComplexMethod")
-    fun getColorForType(type: String?) = when (type) {
-        "fire" -> R.color.fire_type_color
-        "water" -> R.color.water_type_color
-        "grass" -> R.color.grass_type_color
-        "electric" -> R.color.electric_type_color
-        "normal" -> R.color.normal_type_color
-        "fighting" -> R.color.fighting_type_color
-        "flying" -> R.color.flying_type_color
-        "poison" -> R.color.poison_type_color
-        "rock" -> R.color.rock_type_color
-        "ground" -> R.color.ground_type_color
-        "bug" -> R.color.bug_type_color
-        "ghost" -> R.color.ghost_type_color
-        "steel" -> R.color.steel_type_color
-        "psychic" -> R.color.psychic_type_color
-        "ice" -> R.color.ice_type_color
-        "dragon" -> R.color.dragon_type_color
-        "dark" -> R.color.dark_type_color
-        "fairy" -> R.color.fairy_type_color
-        else -> R.color.normal_type_color
+    fun getColorForType(type: String?) = if (typeColorMap.containsKey(type)) {
+        typeColorMap[type] ?: R.color.normal_type_color
+    } else {
+        R.color.normal_type_color
     }
 }
+
+/**
+ * Maps string names for pokemon types to a color resource
+ */
+private val typeColorMap = mapOf(
+    "fire" to R.color.fire_type_color,
+    "water" to R.color.water_type_color,
+    "grass" to R.color.grass_type_color,
+    "electric" to R.color.electric_type_color,
+    "normal" to R.color.normal_type_color,
+    "fighting" to R.color.fighting_type_color,
+    "flying" to R.color.flying_type_color,
+    "poison" to R.color.poison_type_color,
+    "rock" to R.color.rock_type_color,
+    "ground" to R.color.ground_type_color,
+    "bug" to R.color.bug_type_color,
+    "ghost" to R.color.ghost_type_color,
+    "steel" to R.color.steel_type_color,
+    "psychic" to R.color.psychic_type_color,
+    "ice" to R.color.ice_type_color,
+    "dragon" to R.color.dragon_type_color,
+    "dark" to R.color.dark_type_color,
+    "fairy" to R.color.fairy_type_color,
+)
